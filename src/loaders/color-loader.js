3c64c12a649a269d9ad8206a4122f48966867e62
Send_Nukez <erikjulius2701@yahoo.de>
2021-11-03 01:40:54 +0100

    fix color.ini generation

--- src/loaders/color-loader.js
+++ src/loaders/color-loader.js
@@ -1,13 +1,18 @@
-const REGEX = /(?<key>.*?):.*?#(?<color>.*?)/gm;
+const TRIM_REGEX = /colors: \((?<colors>.*?)\);/s;
+const COLOR_REGEX = /(?<key>[\w-]*?):.*?#(?<color>.*?),?$/gm;
 
 module.exports = function (content, map, meta) {
-    const matches = [...content.matchAll(REGEX)];
-    const outLines = ["[base]"];
-
+    const colors = content
+        .match(TRIM_REGEX)
+        .groups.colors.split("\n")
+        .map((l) => l.trim())
+        .join("\n");
+    const matches = [...colors.matchAll(COLOR_REGEX)];
+    const ini = ["[base]"];
     for (let i = 0; i < matches.length; i++) {
         const { key, color } = matches[i].groups;
-        outLines.push(`${key} = ${color}`);
+        ini.push(`${key} = ${color}`);
     }
 
-    return outLines.join("\n");
+    return ini.join("\n");
 };
