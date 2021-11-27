2e1b7a05b56a6a8126595a107ac31dba1d69e2b6
Send_Nukez <erikjulius2701@yahoo.de>
2021-11-27 10:00:31 +0100

    update bug / feature report templates to use comments

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -8,28 +8,32 @@ assignees: ""
 ---
 
 **Describe the bug**
-A clear and concise description of what the bug is.
+<!-- A clear and concise description of what the bug is. -->
 
 **To Reproduce**
 Steps to reproduce the behavior:
+1. 
 
 **Screenshots**
-If applicable, add screenshots to help explain your problem.
+<!-- If applicable, add screenshots to help explain your problem. -->
 
 **Desktop Setup**
- - Operating System: [e.g. Windows 10, MacOS 10.14]
- - Spotify version [e.g. 1.1.71.560] from `Menu (Three dots top left) > Help > About`
- - Spicetify version [e.g. 2.4.1] run `spicetify -v`
- - Dribbblish version [e.g. 3.0.1] from `Dribblish Settings > About`
+ - Operating System: <!-- [e.g. Windows 10, MacOS 10.14] -->
+ - Spotify: <!-- [e.g. 1.1.71.560] from `Menu (Three dots top left) > Help > About` -->
+ - Spicetify: <!-- [e.g. 2.4.1] run `spicetify -v` -->
+ - Dribbblish: <!-- [e.g. 3.0.1] from `Dribblish Settings > About` -->
 
 **Logs**
-Add logs from console. To do that
-1. Run `spicetify enable-devtool` in terminal
-2. Spotify will be restarted
-3. Hit <kbd>Ctrl + Shift + I</kbd> to open DevTools window
-4. Navigate to tab Console
-5. Copy console window content.
+<!--
+    Add logs from console. To do that
+    1. Run `spicetify enable-devtool` in terminal
+    2. Spotify will be restarted
+    3. Hit <kbd>Ctrl + Shift + I</kbd> to open DevTools window
+    4. Navigate to tab Console
+    5. Copy console window content.
+-->
 
+<!-- Paste logs below or attach a screenshot -->
 ```console
-(Please paste here console logs or attach a screenshot)
+
 ```
--- src/js/main.js
+++ src/js/main.js
@@ -357,45 +357,55 @@ Dribbblish.on("ready", () => {
         data: "Create Report",
         onChange: () => {
             const reportBody = `
-                    **Describe the bug**
-                    A clear and concise description of what the bug is.
-                    
-                    **To Reproduce**
-                    Steps to reproduce the behavior:
-                    
-                    **Screenshots**
-                    If applicable, add screenshots to help explain your problem.
-    
-                    **Logs**
+                **Describe the bug**
+                <!-- A clear and concise description of what the bug is. -->
+                
+                **To Reproduce**
+                Steps to reproduce the behavior:
+                1. 
+                
+                **Screenshots**
+                <!-- If applicable, add screenshots to help explain your problem. -->
+                
+                **Desktop Setup**
+                - Operating System: <!-- [e.g. Windows 10, MacOS 10.14] -->
+                - Spotify: <!-- [e.g. 1.1.71.560] from \`Menu (Three dots top left) > Help > About\` -->
+                - Spicetify: <!-- [e.g. 2.4.1] run \`spicetify -v\` -->
+                - Dribbblish: <!-- [e.g. 3.0.1] from \`Dribblish Settings > About\` -->
+                
+                **Logs**
+                <!--
                     Add logs from console. To do that
                     1. Run \`spicetify enable-devtool\` in terminal
                     2. Spotify will be restarted
                     3. Hit <kbd>Ctrl + Shift + I</kbd> to open DevTools window
                     4. Navigate to tab Console
                     5. Copy console window content.
-    
-                    \`\`\`console
-                    (Please paste here console logs or attach a screenshot)
-                    \`\`\`
-    
-                    ---
-    
-                    ### Info for Contributors:
-                    
-                    **Versions**
-                    ${Dribbblish.config.getOptions("aboutDribbblishInfo").description}
-    
-                    **Extensions**
-                    ${$(`script[src^="extensions/"]`)
-                        .toArray()
-                        .map((e) => `- ${e.src.split("/").slice(-1)[0]}`)
-                        .join("\n")}
-    
-                    **Settings**
-                    \`\`\`json
-                    ${JSON.stringify(Dribbblish.config.export(), null, 4)}
-                    \`\`\`
-                `
+                -->
+                
+                <!-- Paste logs below or attach a screenshot -->
+                \`\`\`console
+                
+                \`\`\`
+
+                ---
+
+                ### Info for Contributors:
+                
+                **Versions**
+                ${Dribbblish.config.getOptions("aboutDribbblishInfo").description}
+
+                **Extensions**
+                ${$(`script[src^="extensions/"]`)
+                    .toArray()
+                    .map((e) => `- ${e.src.split("/").slice(-1)[0]}`)
+                    .join("\n")}
+
+                **Settings**
+                \`\`\`json
+                ${JSON.stringify(Dribbblish.config.export(), null, 4)}
+                \`\`\`
+            `
                 .split("\n")
                 .map((line) => line.replace(/^ {16}/, ""))
                 .join("\n");
